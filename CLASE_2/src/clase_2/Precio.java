ce51781b204ac9ca5f80f0ed7733658e773a01a1
Jhonathan <jonatanramirezx_123@hotmail.com>
2018-10-12 10:30:16 -0500

    semana 2

---
+++ CLASE_2/src/clase_2/Precio.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase_2;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Precio {
+    double euros;
+
+    public double getEuros() {
+        return 0;
+    }
+
+    public void setEuros(double x) {
+        this.euros = euros;
+    }
+    
+}
